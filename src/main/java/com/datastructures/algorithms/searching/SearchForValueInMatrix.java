58036c983138292475e9636ee77e52dee9de224e
Kavita Jadhav <kavita.jadhav1109@gmail.com>
2020-12-10 15:09:15 -0800

    Update SearchForValueInMatrix.java

--- src/main/java/com/datastructures/algorithms/searching/SearchForValueInMatrix.java
+++ src/main/java/com/datastructures/algorithms/searching/SearchForValueInMatrix.java
@@ -1,4 +1,24 @@
 /*
+Problem: We are given a 2D array where all elements in any individual row or column are sorted. 
+In such a matrix, we have to search or find the position of, a given key. 
+
+Solution: One simple solution is to scan the whole 2D array for the key in O(mn)O(mn) time. 
+However, there are better solutions with less time complexity that use the sorted property of matrix.
+We can use a binary search on each row to find if the key exists in a row or not. 
+The time complexity of this solution is O(m * log n)O(m∗logn). 
+Though it looks good, we have an even better solution.
+
+Here is the algorithm that we are going to use. We start from the upper right corner of the matrix and compare its value with the key.
+If they are equal, we have found the position of the key. 
+If the key is smaller than the current element, we move one position to the left. 
+If the key is larger than the current element, we move one position down. 
+The reason for doing so is because the matrix is sorted, moving left would result in lower values than the current value 
+and moving down would result in higher values than the current value. 
+We continue this process until either we find the element or go out of the boundary of the matrix (which indicates that the key does not exist).
+This solution will visit mm + nn elements at most in the matrix, giving us a time complexity of O(m + n)O(m+n). 
+Also note that we cannot do our searching procedure from the top left or the bottom right corner since in both cases, 
+the keys at either side of that corner are increasing or decreasing respectively. 
+Note that we can start the search from the bottom left corner as well, which would result in similar results as starting from the top right.
 
 Search in Matrix using Divide and conquer
 Runtime complexity #
